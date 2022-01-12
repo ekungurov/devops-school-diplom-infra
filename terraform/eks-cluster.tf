e96f08e1179a40daa410f430b22f100c32b02393
Yevgeny Kungurov <ekungurov83@gmail.com>
2022-01-12 15:44:49 +0400

    Add terraform files

---
+++ terraform/eks-cluster.tf
@@ -0,0 +1,42 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "17.24.0"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.21"
+  vpc_id          = module.vpc.vpc_id
+  subnets         = module.vpc.public_subnets
+
+  manage_worker_iam_resources = false
+  manage_cluster_iam_resources = false
+  cluster_iam_role_name = "eksClusterRole"
+
+  tags = {
+    Environment = "training"
+  }
+
+  workers_group_defaults = {
+    instance_type    = "t3.micro"
+    root_volume_type = "gp2"
+    root_volume_size = "10"
+  }
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t3.small"
+      asg_desired_capacity          = 2
+      asg_max_capacity              = 2
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+      iam_instance_profile_name     = "eksWorkerNodeRole"
+    },
+  ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
---
+++ terraform/rds.tf
@@ -0,0 +1,39 @@
+resource "aws_db_subnet_group" "education" {
+  name       = "education"
+  subnet_ids = module.vpc.public_subnets
+
+  tags = {
+    Name = "Education"
+  }
+}
+
+resource "aws_db_parameter_group" "education" {
+  name   = "education"
+  family = "mariadb10.4"
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8"
+  }
+
+  parameter {
+    name  = "character_set_client"
+    value = "utf8"
+  }  
+}
+
+resource "aws_db_instance" "education" {
+  identifier             = "education"
+  instance_class         = "db.t2.micro"
+  allocated_storage      = 20
+  name                   = "star_wars"
+  engine                 = "mariadb"
+  engine_version         = "10.4"
+  username               = "edu"
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.education.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  parameter_group_name   = aws_db_parameter_group.education.name
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+}
