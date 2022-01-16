04ad0007008f82fd5db220334b56bfaa5bbcd46c
Yevgeny Kungurov <ekungurov83@gmail.com>
2022-01-16 16:18:32 +0400

    Enable auto mapping of public IPs in public_subnets

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -25,7 +25,7 @@ module "vpc" {
   enable_nat_gateway   = false
   single_nat_gateway   = true
   enable_dns_hostnames = true
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
 
   tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
