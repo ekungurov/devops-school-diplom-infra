4895cb414d0b2461cb3d4a8c048f264efa4a20ef
Yevgeny Kungurov <ekungurov83@gmail.com>
2022-01-17 17:31:03 +0400

    Add custom aws-auth configmap via terraform

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -10,6 +10,7 @@ module "eks" {
   manage_worker_iam_resources = false
   manage_cluster_iam_resources = false
   cluster_iam_role_name = "eksClusterRole"
+  manage_aws_auth = false
 
   tags = {
     Environment = "training"
@@ -32,6 +33,34 @@ module "eks" {
     },
   ]
 }
+
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name             = "aws-auth"
+    namespace        = "kube-system"
+    labels           = {
+      "app.kubernetes.io/managed-by" = "Terraform"
+      "terraform.io/module"          = "root"
+    }
+  }
+
+  data = {
+    mapAccounts   = jsonencode([])
+    mapRoles      = <<EOT
+- rolearn: "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/eksWorkerNodeRole"
+  username: system:node:{{EC2PrivateDNSName}}
+  groups:
+  - system:bootstrappers
+  - system:nodes
+  EOT
+    mapUsers      = <<EOT
+- userarn: "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/yk-eks-developer"
+  username: eks-release-engineer
+  EOT
+  }
+}
+
+data "aws_caller_identity" "current" {}
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
